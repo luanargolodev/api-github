d4f3069f2dc4833a005278ecb5e387cf7660262d
Luan Argolo <ipsluan@icloud.com>
2022-04-28 12:22:24 -0300

    :tada: Commit inicial

--- src/App.js
+++ src/App.js
@@ -3,28 +3,20 @@ import Header from './components/Header';
 import Button from './components/Button';
 
 const App = () => {
-  const [piada, setPiada] = useState('1');
-  const [showPiada, setShowPiada] = useState(false);
-
   function handleClick(event) {
     event.preventDefault();
-    setShowPiada(!showPiada);
 
     fetch('https://api-charadas.herokuapp.com/puzzle?lang=ptbr', {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
+      mode: 'no-cors',
     })
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => console.log(data.question))
       .catch((err) => console.log(err));
   }
 
   return (
     <div className="animeLeft container">
-      <Header title="Gerador de Charadas" />
+      <Header title="Busque um perfil no GitHub" />
       <Button onClick={handleClick}>Gerar Charada</Button>
     </div>
   );
